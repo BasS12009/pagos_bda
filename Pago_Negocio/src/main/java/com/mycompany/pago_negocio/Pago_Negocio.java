9c498f0e01d3935878081a6348f22a1d2b016aa5
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-06-18 10:20:29 -0700

    Proyectos

---
+++ Pago_Negocio/src/main/java/com/mycompany/pago_negocio/Pago_Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pago_negocio;
+
+/**
+ *
+ * @author diana
+ */
+public class Pago_Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
