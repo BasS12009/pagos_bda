9c6859444f464c474b6eacc07bef105869dacc1f
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-06-18 21:56:01 -0700

    dto

--- Pago_Negocio/src/main/java/DTOs/TiposDTO.java
+++ Pago_Negocio/src/main/java/DTOs/TiposDTO.java
@@ -15,4 +15,48 @@ public class TiposDTO {
     private String nombre;
     private Integer numeroParcialidades;
     private List<PagoDTO> pagos;
+
+    public TiposDTO() {
+    }
+
+    public TiposDTO(Long id, String nombre, Integer numeroParcialidades, List<PagoDTO> pagos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.numeroParcialidades = numeroParcialidades;
+        this.pagos = pagos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNumeroParcialidades() {
+        return numeroParcialidades;
+    }
+
+    public void setNumeroParcialidades(Integer numeroParcialidades) {
+        this.numeroParcialidades = numeroParcialidades;
+    }
+
+    public List<PagoDTO> getPagos() {
+        return pagos;
+    }
+
+    public void setPagos(List<PagoDTO> pagos) {
+        this.pagos = pagos;
+    }
+    
+    
 }
