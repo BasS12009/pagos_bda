c29f68bf0439f401c01cfc3254400940505d3e19
Chavirez <santiagosanchezch@gmail.com>
2024-06-25 03:46:46 -0700

    Final Commit
    Final commit XFAVOR

--- Pago_Persistencia/target/generated-sources/annotations/entidades/Tipos_.java
+++ Pago_Persistencia/target/generated-sources/annotations/entidades/Tipos_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-25T03:42:19", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-25T03:45:23", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Tipos.class)
 public class Tipos_ { 
 
